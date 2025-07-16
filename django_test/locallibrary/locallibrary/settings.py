7040c24b80c00e2a22cd8cf3cd25adf8278c3156
sowmiya0406-t <sowmiyat791@gmail.com>
2025-07-16 09:54:34 +0530

    Create crud_operations for locallibrary

--- django_test/locallibrary/locallibrary/settings.py
+++ django_test/locallibrary/locallibrary/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'catalog.apps.CatalogConfig', 
+    'django.contrib.staticfiles', 
+    'crud_operations',
 ]
 
 MIDDLEWARE = [
